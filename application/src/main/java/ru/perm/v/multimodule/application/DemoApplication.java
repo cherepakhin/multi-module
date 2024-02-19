1edcfcde9f9a65900734b0509d0144643e433220
Черепахин Василий <vasi.che@gmail.com>
2024-02-19 20:19:50 +0500

    move to package ru.perm.v...

---
+++ application/src/main/java/ru/perm/v/multimodule/application/DemoApplication.java
@@ -0,0 +1,27 @@
+package ru.perm.v.multimodule.application;
+
+import ru.perm.v.multimodule.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "ru.perm.v.multimodule")
+@RestController
+public class DemoApplication {
+
+	private final MyService myService;
+
+	public DemoApplication(MyService myService) {
+		this.myService = myService;
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return myService.message();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
